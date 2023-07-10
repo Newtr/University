c95632d37b76eee90b809a1fed8c4ddef6dd3358
Roman <92608037+Newtr@users.noreply.github.com>
2023-07-10 19:21:37 +0300

    Add files via upload

---
+++ Course 2/DP/3 Lab/Lab 3/Lec03LibN/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Lec03LibN
+{
+    static public partial class Lec03BLib
+    {
+        static public partial IFactory getL1()
+        {
+            IFactory factory = null;
+            IBonus bonus = null;
+            IBonus getA(float cost1hour)
+            {
+                bonus.cost1hour = cost1hour;
+
+            }
+
+        }
+        static public partial IFactory getL2(float a)
+        {
+            IFactory factory = null;
+            return factory;
+        }
+        static public partial IFactory getL3(float a, float b)
+        {
+            IFactory factory = null;
+            return factory;
+        }
+    }
+}
---
+++ Course 2/DP/3 Lab/Lab 3/Lec03LibN/Class_info.cs
@@ -0,0 +1,35 @@
+﻿namespace Lec03LibN
+{
+    public interface IBonus // вознаграждение
+    {
+        float cost1hour { get; set; }   // стоимость одного часа
+        float calc(float number_hours);  // вычисление возраграждения - количество отработанных часов
+    }
+
+    public interface IFactory   // типы вознаграждения
+    {
+        IBonus getA(float cost1hour);   // тип вознаграждения А
+        IBonus getB(float cost1hour, float x);  // тип вознаграждения B
+        IBonus getC(float cost1hour, float x, float y); // тип вознаграждения С
+    }
+
+    static public partial class Lec03BLib
+    {
+        static public partial IFactory getL1();
+        static public partial IFactory getL2(float a);
+        static public partial IFactory getL3(float a, float b);
+    }
+
+    public class Employee
+    {
+        public IBonus bonus { get; private set; }
+        public Employee(IBonus bonus)
+        {
+            this.bonus = bonus;
+        }
+        public float calcBonus(float number_hours) 
+        {
+            return bonus.calc(number_hours);
+        }
+    }
+}
