9ae3de7d49f5d7b1ba32fe80ea82c8dc9bc282f2
Roman <92608037+Newtr@users.noreply.github.com>
2023-07-10 18:43:08 +0300

    Add files via upload

---
+++ Course 2/PNA/6 Lab/ServerNP/ClientNPct/ClientNPct.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <clocale>
+#include <ctime>
+
+#include "ErrorMsgText.h"
+#include "Windows.h"
+
+#define STOP "STOP"
+#define NAME L"\\\\.\\pipe\\Tube"
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    HANDLE cH;
+    DWORD lp;
+    char ibuf[50], obuf[50] = "Hello from ClientNPct ";
+
+    try {
+        cout << "ClientNPсt\n\n"; 
+
+            if (!CallNamedPipe(NAME,
+                obuf,  sizeof(obuf), 
+                ibuf, sizeof(ibuf), 
+                &lp, 
+                NULL))
+            {                  
+                throw SetPipeError("CallNamedPipe: ", GetLastError());
+            }
+            cout << ibuf << endl;
+
+        system("pause");
+    }
+    catch (string ErrorPipeText) {
+        cout << endl << ErrorPipeText;
+    }
+}
