cc87080711d4317e1dc5d80ba3da4dec4011853a
Roman <92608037+Newtr@users.noreply.github.com>
2023-09-14 08:26:31 +0300

    Add files via upload

--- Course 1/C++ Project/Project.cpp
+++ Course 1/C++ Project/Project.cpp
@@ -1,309 +1,176 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 using namespace std;
 
-struct Transport{
-    float Speed;
-    float TicketCost;
-    int Time;
-    int MaxRange;
+struct Student
+{
+    string Name;
+    float eng_mark, math_mark, phys_mark, sci_mark, art_mark;
+    double average_mark;
+    string status;
+
+    void GetInfo();
+    void Calculate();
+    void ShowData();
 };
 
-/* int Transport::Calculate(enum CRange){
-    TicketCost = MaxRange * CRange;
-    Time = MaxRange/Speed;
-    return Time,TicketCost;
-} */
-
-enum TransportClass{
-    CAR = 4,
-    BUS = 90,
-    MINIBUS = 20,
-    TRAIN = 50,
-    BIKE = 2,
-    ONFOOT = 0,
-};
-
-enum CitiesRange{
-    MINSK_BREST = 350,
-    MINSK_GRODNO = 270,
-    MINSK_VITEBSK = 310,
-    MINSK_GOMEL = 320,
-    MINSK_MOGILEV = 210,
-
-    BREST_GRODNO = 240,
-    BREST_VITEBSK = 614,
-    BREST_GOMEL = 576,
-    BREST_MOGILEV = 523,
-
-    GRODNO_VITEBSK = 536,
-    GRODNO_GOMEL = 594,
-    GRODNO_MOGILEV = 478,
-
-    VITEBSK_GOMEL = 331,
-    VITEBSK_MOGILEV = 161,
-
-    GOMEL_MOGILEV = 174
-};
-
-void Calculate(Transport& ex, CitiesRange cit, int spd, TransportClass tr){
-    ex.MaxRange = cit;
-    ex.Speed = spd;
-    ex.Time = (ex.MaxRange/ex.Speed)*60;
-    ex.TicketCost = ex.MaxRange/tr;
+void Student::Calculate(){
+    average_mark = (eng_mark+math_mark+phys_mark+sci_mark+art_mark)/5;
+    if (average_mark>8)
+    {
+        status = "Best";
+    }
+    else if (average_mark>6)
+    {
+        status = "Good";
+    }
+    else if (average_mark>5)
+    {
+        status = "Bad";
+    }
+    else 
+    {
+        status = "Awful";
+    }
 }
 
-int TakeSpeed(Transport tr, int choose){
+void Student::GetInfo(){
+    cout<<"\nStudent name: ";
+    cin>>Name;
+    cout<<"\nEnter marks in English: ";
+    cin>>eng_mark;
+    cout<<"\nEnter marks in Math: ";
+    cin>>math_mark;
+    cout<<"\nEnter marks in Physics: ";
+    cin>>phys_mark;
+    cout<<"\nEnter marks in Science: ";
+    cin>>sci_mark;
+    cout<<"\nEnter marks in Art: ";
+    cin>>art_mark;
+    Calculate();
+}
+
+void Student::ShowData(){
+    cout<<"Student name: "<<Name;
+    cout<<"English: "<<eng_mark;
+    cout<<"Math: "<<math_mark;
+    cout<<"Physics: "<<phys_mark;
+    cout<<"Science: "<<sci_mark;
+    cout<<"Art: "<<art_mark;
+    cout<<"Status of student: "<<status;
+}
+
+void CreateStudent()
+{
+    Student stud;
+    ofstream OFile;
+    OFile.open("Student.txt", ios::binary|ios::app);
+    stud.GetInfo();
+    OFile<<"Name: "<<stud.Name
+    <<" English: "<<stud.eng_mark
+    <<" Math: "<<stud.math_mark
+    <<" Physics: "<<stud.phys_mark
+    <<" Science: "<<stud.sci_mark
+    <<" Art: "<<stud.art_mark
+    <<" Status: "<<stud.status<<endl;
+    OFile.close();
+    cout<<"Student was created successfully!"<<endl;
+}
+
+void ShowAllInfo()
+{ 
+    string str;
+    Student stud;
     ifstream IFile;
-    string str, speed;
-    static int NowSpeed;
-    int count = 19, right=1;
-    bool car = true;
-    IFile.open("D:\\GitHub\\Second-Project\\TransportInformation.txt", ios::binary|ios::app);
-    while (!IFile.eof() && car==true)
+    IFile.open("Student.txt", ios::binary);
+    if (!IFile)
+    {
+        cout<<"ERROR! Press any key to exit ...";
+        cin.ignore();
+        cin.get();
+        return;
+    }
+    cout<<"INFORMATION\n\n";
+    while (!IFile.eof())
+    {
+        getline(IFile,str);
+        cout<<str<<endl;
+        if (str=="")
+        {
+            break;
+        }
+        
+    }
+    
+    IFile.close();
+    cin.ignore();
+    cin.get();
+}
+
+void DeleteStudent(){
+    string str, StudentName, NameInFile;
+    bool NewWord=false;
+    int count=6;
+    ifstream IFile;
+    IFile.open("Student.txt",ios::binary);
+    cout<<"Enter name of student: ";
+    cin>>StudentName;
+    ofstream OFile;
+    OFile.open("temp.txt",ios::binary|ios::app);
+    while (!IFile.eof() && NewWord==false)
     {
         getline(IFile,str);
         if (str=="")
         {
-            car=false;
-            continue;
-        }
-        else if (right==choose)
-        {
-            switch (choose)
-            {
-            case 1:
-                speed = str.substr(17,2);
-                break;
-            case 2:
-                speed = str.substr(17,2);
-                break;
-            case 3:
-                speed = str.substr(21,2);
-                break;
-            case 4:
-                speed = str.substr(19,2);
-                break;
-            case 5:
-                speed = str.substr(18,2);
-                break;
-            case 6:
-                speed = str.substr(20,1);
-                break;
-            
-            }
-            car = false;
-        }
-        else if (right!=choose)
-        {
-            right++;
+            NewWord=true;
             continue;
         }
         
+        while (str.at(count)!=' ')
+        {
+            NameInFile+=str.at(count);
+            count++;
+        }
+        if (NameInFile==StudentName)
+        {
+            continue;
+        }
+        else if (NameInFile!=StudentName)
+        {
+        OFile<<str<<endl;
+        NameInFile="";
+        count=6;
+        }
     }
-    NowSpeed = stoi(speed);
+    OFile.close();
     IFile.close();
-    return NowSpeed;
-}
+    remove("Student.txt");
+    rename("temp.txt","Student.txt");
+    cout<<"All good!";
 
-void TakeIndex(string ff, int& Index){
-    if (ff.substr(0)=="Minsk")
-    {
-        Index = 220000;
-    }
-    else if (ff.substr(0)=="Brest")
-    {
-        Index = 224000;
-    }
-    else if (ff.substr(0)=="Grodno")
-    {
-        Index = 230000;
-    }
-    else if (ff.substr(0)=="Vitebsk")
-    {
-        Index = 210000;
-    }
-    else if (ff.substr(0)=="Gomel")
-    {
-        Index = 246000;
-    }
-    else if (ff.substr(0)=="Mogilev")
-    {
-        Index = 212000;
-    }
-}
-
-void FindCityRange(int Ind1, int Ind2, CitiesRange& Rg){
-    string City1, City2, AllCity;
-    switch (Ind1)
-    {
-    case 220000:    //Minsk
-        switch (Ind2)
-        {
-        case 224000:
-            Rg = MINSK_BREST;
-            break;
-        case 230000:
-            Rg = MINSK_GRODNO;
-            break;
-        case 210000:
-            Rg = MINSK_VITEBSK;
-            break;
-        case 246000:
-            Rg = MINSK_GOMEL;
-            break;
-        case 212000:
-            Rg = MINSK_MOGILEV;
-            break;
-        }
-        break;
-    case 224000:    //Brest
-        switch (Ind2)
-        {
-        case 220000:
-            Rg = MINSK_BREST;
-            break;
-        case 230000:
-            Rg = BREST_GRODNO;
-            break;
-        case 210000:
-            Rg = BREST_VITEBSK;
-            break;
-        case 246000:
-            Rg = BREST_GOMEL;
-            break;
-        case 212000:
-            Rg = BREST_MOGILEV;
-            break;
-        }
-        break;
-    case 230000:    //Grodno
-        switch (Ind2)
-        {
-        case 220000:
-            Rg = MINSK_GRODNO;
-            break;
-        case 224000:
-            Rg = BREST_GRODNO;
-            break;
-        case 210000:
-            Rg = GRODNO_VITEBSK;
-            break;
-        case 246000:
-            Rg = GRODNO_GOMEL;
-            break;
-        case 212000:
-            Rg = GRODNO_MOGILEV;
-            break;
-        }
-        break;
-    case 210000:    //Vitebsk
-        switch (Ind2)
-        {
-        case 20000:
-            Rg = MINSK_VITEBSK;
-            break;
-        case 224000:
-            Rg = BREST_VITEBSK;
-            break;
-        case 230000:
-            Rg = GRODNO_VITEBSK;
-            break;
-        case 246000:
-            Rg = VITEBSK_GOMEL;
-            break;
-        case 212000:
-            Rg = VITEBSK_MOGILEV;
-            break;
-        }
-        break;
-    case 246000:    //Gomel
-        switch (Ind2)
-        {
-        case 20000:
-            Rg = MINSK_GOMEL;
-            break;
-        case 224000:
-            Rg = BREST_GOMEL;
-            break;
-        case 230000:
-            Rg = GRODNO_GOMEL;
-            break;
-        case 246000:
-            Rg = VITEBSK_GOMEL;
-            break;
-        case 212000:
-            Rg = GOMEL_MOGILEV;
-            break;
-        }
-        break;
-    case 212000:    //Mogilev
-        switch (Ind2)
-        {
-        case 220000:
-            Rg = MINSK_MOGILEV;
-            break;
-        case 224000:
-            Rg = BREST_MOGILEV;
-            break;
-        case 230000:
-            Rg = GRODNO_MOGILEV;
-            break;
-        case 246000:
-            Rg = VITEBSK_MOGILEV;
-            break;
-        case 212000:
-            Rg = GOMEL_MOGILEV;
-            break;
-        }
-        break;
-    }
-}
-
-void FindMyTransp(TransportClass& Tr, int userchoose){
-    switch (userchoose)
-    {
-    case 1:
-        Tr = CAR;
-        break;
-    case 2:
-        Tr = BUS;
-        break;
-    case 3:
-        Tr = MINIBUS;
-        break;
-    case 4:
-        Tr = TRAIN;
-        break;
-    case 5:
-        Tr = BIKE;
-        break;
-    case 6:
-        Tr = ONFOOT;
-        break;
-    }
 }
 
 int main(){
-    int choose, CityIndex1=0, CityIndex2=0, speed;
-    Transport MyTransport;
-    string from, to;
-    CitiesRange Range;
-    TransportClass Transp;
-    cout<<"Choose vehicle: 1) Car 2) Bus 3) Minibus 4) Train 5) Bike 6) On Foot"<<endl;
-    cin>>choose;
-    FindMyTransp(Transp, choose);
-    speed = TakeSpeed(MyTransport,choose);
-    cout<<"You going from : ";  cin>>from;
-    cout<<"Going to : ";    cin>>to;
-    TakeIndex(from,CityIndex1);
-    TakeIndex(to,CityIndex2);
-/*     cout<<"1 = "<<CityIndex1<<" 2 = "<<CityIndex2<<endl; */
-    FindCityRange(CityIndex1,CityIndex2,Range);
-/*     cout<<"Vot = "<<Range<<endl; */
-    Calculate(MyTransport,Range,speed,Transp);
-    cout<<"Примерное время составит: "<<MyTransport.Time<<" минут"<<endl;
-    cout<<"Стоимость составит: "<<MyTransport.TicketCost<<"$"<<endl;
+    int UserChoose;
+    do{
+    cout<<"1)Create Student"<<endl;
+    cout<<"2)Show All Information about students"<<endl;
+    cout<<"3)Delete Student"<<endl;
+    cout<<"4)Exit"<<endl;
+    cout<<"Your choose : "; cin>>UserChoose;
+    switch (UserChoose)
+    {
+    case 1:
+        CreateStudent();
+        break;
+    case 2:
+        ShowAllInfo();
+        break;
+    case 3:
+        DeleteStudent();
+        break;
+    }
+    }while(UserChoose!=4);
+    return 0;
 }
